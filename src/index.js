0babc381553952d87a560297b240286511c53a8f
Kiana Long <kianajade42.kl@gmail.com>
2021-04-15 12:48:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ function loadImages()
 { const imgUrl = "https://dog.ceo/api/breeds/image/random/4"}
  fetch(imgUrl)
  .then(res=> res.json())
-.then (results => { results.message.forEach(image => addImage(image))});
+ .then(results => { results.message.forEach(image => addImage(image))});
 }
  function addImage(dogPicUrl) {
    let container= document.querySelector('#dog-image-container');
    let newImageEl= document.createElement('img');
-   newImageel.src=dogPicUrl
+   newImageEl.src=dogPicUrl
    container.appendChild(newImageEl);
  }
  function loadBreedOptions() {
